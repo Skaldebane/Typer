2798ecfb13ff0f4a81dd0de54b587a3febe6c525
Skaldebane <skaldebane@gmail.com>
2019-12-21 13:52:43 +0100

    The first silly test.

--- app/src/main/java/com/skaldebane/typer/MainActivity.java
+++ app/src/main/java/com/skaldebane/typer/MainActivity.java
@@ -2,7 +2,12 @@ package com.skaldebane.typer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 @SuppressWarnings({"RedundantCast", "ConstantConditions"})
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.appbar_layout);
+
+        final EditText name = (EditText) findViewById(R.id.name);
+        final Button go = (Button) findViewById(R.id.go);
+
+        go.setOnClickListener(v -> {
+            if (name.length() == 0) {
+                Toast.makeText(this, "Come on we're not kidding write your name.", Toast.LENGTH_LONG).show();
+            } else if (name.length() < 3) {
+                Toast.makeText(this, "Is this a chinese name or what???", Toast.LENGTH_LONG).show();
+            } else {
+                Intent i = new Intent(Intent.ACTION_VIEW).setClass(MainActivity.this, TypingActivity.class);
+                i.putExtra("n", name.getText().toString());
+                startActivity(i);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/skaldebane/typer/TypingActivity.java
+++ app/src/main/java/com/skaldebane/typer/TypingActivity.java
@@ -1,16 +1,69 @@
 package com.skaldebane.typer;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+@SuppressWarnings({"ConstantConditions", "RedundantCast"})
+@SuppressLint("SetTextI18n")
 public class TypingActivity extends AppCompatActivity {
+
+    int i1, i2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_typing);
 
+        final TextView typeView = (TextView) findViewById(R.id.typeView);
+
+        String name = getIntent().getStringExtra("n");
+        String line1 = "Hello, " + name + ".";
+        String line2;
+        if (name.toLowerCase().equals("imane")) {
+            line2 = "Such a sweet visitor!";
+        } else if (name.toLowerCase().equals("joseph")) {
+            line2 = "Python personally!";
+        } else if (name.toLowerCase().equals("hossam")) {
+            line2 = "You created me!!";
+        } else {
+            line2 = "That was another test.";
+        }
+
+        i1 = 0;
+        final Timer t1 = new Timer();
+        t1.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(() -> {
+                    if (i1 < line1.length()) {
+                        typeView.setText(typeView.getText().toString() + line1.charAt(i1));
+                        i1++;
+                    } else {
+                        t1.cancel();
+                        typeView.setText(typeView.getText().toString() + "\n");
+                        i2 = 0;
+                        final Timer t2 = new Timer();
+                        t2.scheduleAtFixedRate(new TimerTask() {
+                            @Override
+                            public void run() {
+                                runOnUiThread(() -> {
+                                    if (i2 < line2.length()) {
+                                        typeView.setText(typeView.getText().toString() + line2.charAt(i2));
+                                        i2++;
+                                    } else t2.cancel();
+                                });
+                            }
+                        }, 75, 75);
+                    }
+                });
+            }
+        }, 75, 75);
     }
 
 }
