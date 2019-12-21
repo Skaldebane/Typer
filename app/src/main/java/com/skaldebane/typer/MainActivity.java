5bbb29a26a6229d64faf543d7ef4be684f0bb560
Skaldebane <skaldebane@gmail.com>
2019-12-21 14:46:36 +0100

    A complete revamp of activities.

--- app/src/main/java/com/skaldebane/typer/MainActivity.java
+++ app/src/main/java/com/skaldebane/typer/MainActivity.java
@@ -1,39 +1,29 @@
 package com.skaldebane.typer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-@SuppressWarnings({"RedundantCast", "ConstantConditions"})
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        getSupportActionBar().setCustomView(R.layout.appbar_layout);
-
-        final EditText name = (EditText) findViewById(R.id.name);
-        final Button go = (Button) findViewById(R.id.go);
-
-        go.setOnClickListener(v -> {
-            if (name.length() == 0) {
-                Toast.makeText(this, "Come on we're not kidding write your name.", Toast.LENGTH_LONG).show();
-            } else if (name.length() < 3) {
-                Toast.makeText(this, "Is this a chinese name or what???", Toast.LENGTH_LONG).show();
-            } else {
-                Intent i = new Intent(Intent.ACTION_VIEW).setClass(MainActivity.this, TypingActivity.class);
-                i.putExtra("n", name.getText().toString());
-                startActivity(i);
-            }
-        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.settings, new SettingsFragment()).commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setCustomView(R.layout.appbar_layout);
+        }
     }
 
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
 }
